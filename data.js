e7861cc879d3075a3888fdd380ba0d504f2a3ef5
friarobaz <59922807+friarobaz@users.noreply.github.com>
2020-11-30 15:53:11 +0100

    test

--- data.js
+++ data.js
@@ -33,5 +33,27 @@ const exos = [
         name : "machine",
         url : "http://www.w3schools.com/html/movie.mp4",
         category : "mobilisation"
+    },
+    exo7 = {
+        name : "coude",
+        url : "videos/coude.mp4",
+        category : "articulations"
+    },
+    exo8 = {
+        name : "escalade",
+        url : "videos/escalade.mp4",
+        category : "mobilisation"
+    },
+
+    exo9 = {
+        name : "courir",
+        url : "videos/courir.mp4",
+        category : "raise"
+    },
+
+    exo10 = {
+        name : "machine",
+        url : "http://www.w3schools.com/html/movie.mp4",
+        category : "mobilisation"
     }
 ];
