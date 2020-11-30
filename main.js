50c78b698bae7372847a737030bec351da1e5f8d
friarobaz <59922807+friarobaz@users.noreply.github.com>
2020-11-30 17:07:18 +0100

    cqs

--- main.js
+++ main.js
@@ -7,15 +7,46 @@ function unique(array) {
     });
 }
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+function get_random_color(i){
+    var randHigh = Math.random() * 200 + 55;
+    var randLow2 = 255;
+    var randLow = Math.random() * 30;
+    var randArray = [randHigh, randLow, randLow2];
+    var shuffled = shuffle(randArray);
+    console.log(shuffled);
+
+    return "rgb("+shuffled[0]+","+shuffled[1]+","+shuffled[2]+")";
+};
+
 function display_menu(){
     for (i = 0; i < categories.length; i++){
         var relevant_exos = exos.filter(exo => exo.category === categories[i]);
-        $("#home_page").append("<div class='category' id='"+categories[i]+"'></div>"); //make div for category
+        $("#home_page").append('<div class="category" style="background-color: '+get_random_color(i)+'" id="'+categories[i]+'"></div>'); //make div for category
         $("#"+categories[i]).append(categories[i]); //print category name in div
         for (let j = 0; j < relevant_exos.length; j++) { //print exos in div
-            $("#"+categories[i]).append('<div class="exo" id="'+relevant_exos[j].name+'" onclick="select_exo(\''+relevant_exos[j].name +'\')">'+relevant_exos[j].name+'</div>'); 
+            $("#"+categories[i]).append('<div class="exo" style="background-color: '+get_random_color(i)+'" id="'+relevant_exos[j].name+'" onclick="select_exo(\''+relevant_exos[j].name +'\')">'+relevant_exos[j].name+'</div>'); 
         }
     };
+    //$("#raise").css('background-color', get_random_color());
 };
 
 function select_exo(name){
