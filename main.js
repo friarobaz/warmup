ac453f7db5dc8c51567d3846e7851a6d4347e37a
friarobaz <59922807+friarobaz@users.noreply.github.com>
2020-11-30 15:30:26 +0100

    test

--- main.js
+++ main.js
@@ -10,12 +10,11 @@ function unique(array) {
 function display_menu(){
     for (i = 0; i < categories.length; i++){
         var relevant_exos = exos.filter(exo => exo.category === categories[i]);
-        $("#home_page").append("<div id='"+categories[i]+"'></div>"); //make div for category
-        $("#"+categories[i]).append("<b>"+categories[i] + "</b><br>"); //print category name in div
+        $("#home_page").append("<div class='category' id='"+categories[i]+"'></div>"); //make div for category
+        $("#"+categories[i]).append(categories[i]); //print category name in div
         for (let j = 0; j < relevant_exos.length; j++) { //print exos in div
-            $("#"+categories[i]).append('<div id="'+relevant_exos[j].name+'" onclick="select_exo(\''+relevant_exos[j].name +'\')">'+relevant_exos[j].name+'</div>'); 
+            $("#"+categories[i]).append('<div class="exo" id="'+relevant_exos[j].name+'" onclick="select_exo(\''+relevant_exos[j].name +'\')">'+relevant_exos[j].name+'</div>'); 
         }
-        $("#home_page").append("<br>"); 
     };
 };
 
