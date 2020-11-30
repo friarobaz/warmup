4bf7519539cbf0029870d0073cbb8bc69ef8a6b0
friarobaz <59922807+friarobaz@users.noreply.github.com>
2020-11-30 13:36:40 +0100

    videos

--- data.js
+++ data.js
@@ -1,20 +1,37 @@
 
 const exos = [
     exo1 = {
-        name : "coude",
-        url : "www",
+        name : "papillon",
+        url : "http://www.w3schools.com/html/mov_bbb.mp4",
         category : "articulations"
     },
 
     exo2 = {
-        name : "courir",
-        url : "www2",
+        name : "ours",
+        url : "http://www.w3schools.com/html/movie.mp4",
         category : "raise"
     },
 
     exo3 = {
         name : "genou",
-        url : "www3",
+        url : "http://www.w3schools.com/html/mov_bbb.mp4",
         category : "articulations"
+    },
+    exo4 = {
+        name : "poignets",
+        url : "http://www.w3schools.com/html/mov_bbb.mp4",
+        category : "articulations"
+    },
+
+    exo5 = {
+        name : "squat",
+        url : "http://www.w3schools.com/html/movie.mp4",
+        category : "raise"
+    },
+
+    exo6 = {
+        name : "machine",
+        url : "http://techslides.com/demos/sample-videos/small.mp4",
+        category : "mobilisation"
     }
 ];
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const selected_exos = [];
+const categories = unique(exos.map(({ category }) => category));
 
 function unique(array) {
     return $.grep(array, function(el, index) {
@@ -5,16 +7,54 @@ function unique(array) {
     });
 }
 
-var categories = unique(exos.map(({ category }) => category));
-
-
-for (i = 0; i < categories.length; i++){
-    var relevant_exos = exos.filter(exo => exo.category === categories[i]);
-    $("#home_page").append("<b>"+categories[i] + "</b><br>"); 
-    for (let i = 0; i < relevant_exos.length; i++) {
-        $("#home_page").append(relevant_exos[i].name + "<br>"); 
-    }
-    $("#home_page").append("<br>"); 
+function display_menu(){
+    for (i = 0; i < categories.length; i++){
+        var relevant_exos = exos.filter(exo => exo.category === categories[i]);
+        $("#home_page").append("<div id='"+categories[i]+"'></div>"); //make div for category
+        $("#"+categories[i]).append("<b>"+categories[i] + "</b><br>"); //print category name in div
+        for (let j = 0; j < relevant_exos.length; j++) { //print exos in div
+            $("#"+categories[i]).append('<div id="'+relevant_exos[j].name+'" onclick="select_exo(\''+relevant_exos[j].name +'\')">'+relevant_exos[j].name+'</div>'); 
+        }
+        $("#home_page").append("<br>"); 
+    };
 };
+
+function select_exo(name){
+    var selected_exo = exos.find(exo => exo.name === name)
+    $("#"+selected_exo.category).hide();
+    selected_exos.push(selected_exo);
+    if (selected_exos.length == categories.length) { //if all exos selected
+        play();
+    }
+};
+
+function play(){
+    $("#home_page").hide();
+    $("#player").show();
+    var selected_videos = [];
+    for (let i = 0; i < selected_exos.length; i++) { // fill selected_videos with urls from selected_exos
+        selected_videos.push(selected_exos[i].url)
+    }
+
+    var myvid = document.getElementById('video');
+    var activeVideo = 0;
+    myvid.src = selected_videos[0]; // prepare 1st video and wait for user to play
+
+    myvid.addEventListener('ended', function(e) { // when 1st video is done
+        if (activeVideo < selected_videos.length-1) { // if not the last video
+            activeVideo++; //get next video
+            myvid.src = selected_videos[activeVideo];
+            myvid.play();
+        } else{ // if last video
+            $("#player").hide();
+            $("#end_page").show();
+        };   
+    });
+};
+
+
+display_menu();
+
+
 
 //map(({ nom }) => nom)
